d07cfd4ea907230c00a2355a7fe8cd07163fa9ae
liulalemx <liulalemx@gmail.com>
2023-04-03 14:42:24 +0300

    initial setup

---
+++ MakeFile
@@ -0,0 +1,2 @@
+all:
+	g++ -Iinclude -Iinclude/sdl -Iinclude/headers -Llib -o Main src/*.cpp -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <SDL.h>
+using namespace std;
+
+int main(int argc, char* argv[]){
+    cout << "This works" << endl;
+    int a;
+    cin >> a;
+
+    return 0;
+}
