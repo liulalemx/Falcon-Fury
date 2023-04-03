33e535d986dbc0ad57eb72e7af434bd8690f0d19
liulalemx <liulalemx@gmail.com>
2023-04-03 16:33:38 +0300

    Updated game class

--- src/game.cpp
+++ src/game.cpp
@@ -1,21 +1,19 @@
-#include "headers/game.hpp"
+#include "game.hpp"
 #include <SDL.h>
 #include <SDL_image.h>
 
-Game::Game()
-:_window(nullptr), _renderer(nullptr), _screenWidth(1024), _screenHeight(600), _gameState(GameState::PLAY){};
-Game::~Game(){};
-
-void Game::run(){
-    init("Falcon", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _screenWidth, _screenHeight, SDL_WINDOW_SHOWN);
-    gameLoop();
-}
-
-void Game::init(const char* title, int x, int y, int w, int h, Uint32 flags){
-    SDL_Init(SDL_INIT_EVERYTHING);
+Game::Game(const char* title, int x, int y, int w, int h, Uint32 flags)
+{
+     SDL_Init(SDL_INIT_EVERYTHING);
 
     _window = SDL_CreateWindow(title, x, y, w, h, flags);
     _renderer = SDL_CreateRenderer(_window, -1, 0);
+};
+
+Game::~Game(){};
+
+void Game::run(){
+    gameLoop();
 }
 
 void Game::gameLoop(){
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include "headers/game.hpp"
+#include "game.hpp"
 using namespace std;
 
 int main(int argc, char* argv[]){
-    Game game;
+   int _screenWidth = 1024;
+   int _screenHeight = 600;
 
-    game.run();
+   Game game("Falcon", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _screenWidth, _screenHeight, SDL_WINDOW_SHOWN);
+
+   game.run();
 
     return 0;
 }
